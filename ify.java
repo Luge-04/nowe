3379c3c3770c612805171a83331bc57eaf277fb0
Luge-04 <maksluze@gmail.com>
2024-10-12 12:57:10 +0200

    drugi Program

--- ify.java
+++ ify.java
@@ -1,11 +1,15 @@
 public class ify {
     public static void main(String[] args) {
-        float temperature = 38.5f;
+        float temperature = 35.5f;
         if (temperature > 36.6f) {
             System.out.println("masz goraczke");
         }
+        else if(temperature >= 36.6f && temperature <= 35.5f){
+            System.out.println("jestes zdrowy");
+
+        }
         else{
-            System.out.println("nie masz");
+            System.out.println("jestes oslabiony");
         }
 
     }
