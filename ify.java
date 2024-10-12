5f1647783efb33da6479bb8ce1ccb128fe1c33a8
Luge-04 <maksluze@gmail.com>
2024-10-12 12:48:47 +0200

    drugi Program

---
+++ ify.java
@@ -0,0 +1,8 @@
+public class ify {
+    public static void main(String[] args) {
+        float temperature = 38.5f;
+        if (temperature > 36.6f) {
+            System.out.println("masz goraczke");
+        }
+    }
+}
